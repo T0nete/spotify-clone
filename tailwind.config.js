1d8bbc89271fc3b0fd0347a6038bcc542f38a926
T0nete <tonbarramon@gmail.com>
2024-02-04 23:16:09 +0100

    define tailwind black colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "default-text": "#b3b3b3",
+        black: {
+          100: "#030303",
+          200: "#050505",
+          300: "#090909",
+          400: "#121212",
+          900: "#000000",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
