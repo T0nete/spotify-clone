616e07f123ea5303a71700d4175039ccf1939617
T0nete <tonbarramon@gmail.com>
2024-02-21 22:31:52 +0100

    add mock playlists

--- src/constants/playlist.ts
+++ src/constants/playlist.ts
@@ -2,6 +2,7 @@ export interface PlayList {
   id: string;
   cover: string;
   title: string;
+  description: string;
   songs: PlayListItem[];
 }
 
@@ -15,6 +16,7 @@ export const playlist: PlayList[] = [
     id: "1",
     cover: "https://via.placeholder.com/64",
     title: "Top 50: España",
+    description: "Your daily update of the most played tracks right now - Spain.",
     songs: [
       {
         id: "1",
@@ -31,12 +33,98 @@ export const playlist: PlayList[] = [
         cover: "https://via.placeholder.com/64",
         title: "Song 3",
       },
+      {
+        id: "4",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 4",
+      },
+      {
+        id: "5",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 5",
+      },
+      {
+        id: "6",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 6",
+      },
+      {
+        id: "7",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 7",
+      },
+      {
+        id: "8",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 8",
+      },
+      {
+        id: "9",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 9",
+      },
+      {
+        id: "10",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 10",
+      },
+      {
+        id: "11",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 11",
+      },
+      {
+        id: "12",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 12",
+      },
+      {
+        id: "13",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 13",
+      },
+      {
+        id: "14",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 14",
+      },
+      {
+        id: "15",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 15",
+      },
+      {
+        id: "16",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 16",
+      },
+      {
+        id: "17",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 17",
+      },
+      {
+        id: "18",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 18",
+      },
+      {
+        id: "19",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 19",
+      },
+      {
+        id: "20",
+        cover: "https://via.placeholder.com/64",
+        title: "Song 20",
+      },
     ],
   },
   {
     id: "2",
     cover: "https://via.placeholder.com/64",
     title: "lofi covers",
+    description: "Chill beats to relax/study to.",
     songs: [
       {
         id: "1",
@@ -59,6 +147,7 @@ export const playlist: PlayList[] = [
     id: "3",
     cover: "https://via.placeholder.com/64",
     title: "LA ÚLTIMA",
+    description: "The last songs of the year.",
     songs: [
       {
         id: "1",
@@ -81,6 +170,7 @@ export const playlist: PlayList[] = [
     id: "4",
     cover: "https://via.placeholder.com/64",
     title: "Top 50: Global",
+    description: "Your daily update of the most played tracks right now - Global.",
     songs: [
       {
         id: "1",
@@ -103,6 +193,7 @@ export const playlist: PlayList[] = [
     id: "5",
     cover: "https://via.placeholder.com/64",
     title: "What Now? with Trevor Noah",
+    description: "What Now? with Trevor Noah",
     songs: [
       {
         id: "1",
@@ -125,6 +216,7 @@ export const playlist: PlayList[] = [
     id: "6",
     cover: "https://via.placeholder.com/64",
     title: "GRX",
+    description: "GRX",
     songs: [
       {
         id: "1",
