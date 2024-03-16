9328339d6cdb43b619f50641317f3ee8617b4790
T0nete <toni.barrantes.montilla64@gmail.com>
2024-03-16 23:04:29 +0100

    Add  responisveness and animation

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -18,21 +18,21 @@ const HomePage: React.FC = () => {
       </section>
       <section>
         <h2 className="text-2xl font-bold text-white">Your shows</h2>
-        <div className="grid grid-cols-1 gap-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
+        <div className="flex flex-wrap justify-center">
           {playLists?.map((item) => (
-            <div className="group flex flex-col items-center gap-2 rounded-md bg-black-200 py-3 hover:bg-black-100">
+            <div className="group m-2 w-48 flex-shrink-0 rounded-md bg-black-200 p-3 hover:bg-black-100">
               <div className="relative">
-                <img src={item.cover} alt={`Image of ${item.title}`} className="h-44 w-44 rounded-sm" />
+                <img src={item.cover} alt={`Image of ${item.title}`} className="my-1 h-44 w-44 rounded-sm" />
                 <SizedButton
                   buttonType="play"
                   buttonSize="md"
                   iconSize="xl"
                   onClick={() => {}}
-                  className="absolute bottom-1 right-1 hidden group-hover:block"
+                  className="animation-fadeInUp absolute bottom-1 right-1 hidden group-hover:block"
                   iconClassName="pl-1"
                 />
               </div>
-              <p className="text-white">{item.title}</p>
+              <p className="my-1 text-white">{item.title}</p>
             </div>
           ))}
         </div>
